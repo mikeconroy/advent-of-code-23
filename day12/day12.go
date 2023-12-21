5b430a82c0aaa11721ea5aefde1f8ae569f82ede
Mike Conroy <mikecon@outlook.com>
2023-12-21 21:29:11 +0000

    Day 12 - Further optimizations but not enough for part 2

--- day12/day12.go
+++ day12/day12.go
@@ -29,7 +29,24 @@ func part1(input []string) string {
 }
 
 func part2(input []string) string {
-	return fmt.Sprint(0)
+	fields := createFields(input)
+	return "0"
+	arrangementsCount := 0
+	for _, field := range fields {
+		ogSprings := make([]rune, len(field.springs))
+		ogDamagedGroups := make([]int, len(field.damagedGroups))
+		copy(ogSprings, field.springs)
+		copy(ogDamagedGroups, field.damagedGroups)
+		for i := 0; i < 4; i++ {
+			field.springs = append(field.springs, '?')
+			field.springs = append(field.springs, ogSprings...)
+			field.damagedGroups = append(field.damagedGroups, ogDamagedGroups...)
+		}
+		field.unknowns = (field.unknowns * 5) + 4
+		fmt.Println("Field:", field, getSpringsAsString(field.springs))
+		arrangementsCount += countValidArrangements(field, 0)
+	}
+	return fmt.Sprint(arrangementsCount)
 }
 
 func countValidArrangements(field Field, springIndex int) int {
@@ -61,15 +78,58 @@ func countValidArrangements(field Field, springIndex int) int {
 		copy(springsHash, field.springs)
 		springsHash[springIndex] = '#'
 		fieldHash := Field{springs: springsHash, damagedGroups: field.damagedGroups, unknowns: field.unknowns - 1}
+
 		springIndex++
 		// fmt.Println("springsDot", getSpringsAsString(springsDot), "springsHash", getSpringsAsString(springsHash))
 		count += countValidArrangements(fieldDot, springIndex)
-		count += countValidArrangements(fieldHash, springIndex)
+
+		// We only need to check for possibly valid for the field we have added a # to.
+		// As adding a . should not affect the possibility of it being valid.
+		// Possible check for validity =
+		// 		len(actual groups) < len(expectedDamagedGroups)
+		// 		&& Groups created so far match the expected values.
+		// This could be further improved as it's not efficient repeatedly checking the same groups...
+		if isPossiblyValid(fieldHash) {
+			count += countValidArrangements(fieldHash, springIndex)
+		}
+
 	} else {
 		// TODO: It may be more efficient here to find the next unknown and calling the function with that index.
 		count += countValidArrangements(field, springIndex+1)
 	}
 	return count
+}
+
+func isPossiblyValid(field Field) bool {
+	count := 0
+	currentIndex := 0
+
+	for _, spring := range field.springs {
+		if spring == '#' {
+			// if len(actualGroups) >= len(field.damagedGroups) && count == 0 {
+			// 	fmt.Println("NOT POSSIBLY VALID too many groups.", getSpringsAsString(field.springs), field.damagedGroups)
+			// 	return false
+			// }
+			count++
+			// fmt.Println(count, currentIndex, getSpringsAsString(field.springs))
+			if currentIndex >= len(field.damagedGroups) || count > field.damagedGroups[currentIndex] {
+				// fmt.Println("NOT POSSIBLY VALID group count exceeded", currentIndex, count, getSpringsAsString(field.springs), field.damagedGroups)
+				return false
+			}
+		} else if spring == '.' {
+			if count > 0 {
+				if count != field.damagedGroups[currentIndex] {
+					return false
+				}
+				currentIndex++
+				count = 0
+			}
+		} else {
+			// When we hit an unknown spring return true as it is hard to reason about validity at this point.
+			return true
+		}
+	}
+	return true
 }
 
 func getSpringsAsString(springs []rune) string {
@@ -80,6 +140,7 @@ func getSpringsAsString(springs []rune) string {
 	return result
 }
 
+// Unused - slices are compared inline of checking validity
 func slicesAreEqual(a []int, b []int) bool {
 	if len(a) != len(b) {
 		return false

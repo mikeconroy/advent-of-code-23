43ef773abd62bc71587018ddae360f3648d22348
Mike Conroy <mikecon@outlook.com>
2023-12-23 22:43:32 +0000

    Day 14 - Part 2

--- day14/day14.go
+++ day14/day14.go
@@ -17,11 +17,48 @@ func part1(input []string) string {
 	return fmt.Sprint(plat.CalculateTotalLoad())
 }
 
+// 1 Cycle = Roll North, West, South, East
+// We can't iterate 1 Billion times so instead we find the a loop
+// and skip iterating the loop.
 func part2(input []string) string {
-	return fmt.Sprint(0)
+
+	plat := loadPlatform(input)
+
+	// Holds the first iteration the value was found at.
+	cycles := make(map[string]int)
+	cycleFound := false
+	for x := 1; x <= 1_000_000_000; x++ {
+		plat.TiltNorth()
+		plat.TiltWest()
+		plat.TiltSouth()
+		plat.TiltEast()
+
+		if !cycleFound {
+			key := plat.Key()
+			if iter, ok := cycles[key]; ok {
+				cycleLen := x - iter
+				remainingCycles := (1_000_000_000 - x) % cycleLen
+				x = 1_000_000_000 - remainingCycles
+				cycleFound = true
+			} else {
+				cycles[key] = x
+			}
+		}
+	}
+	return fmt.Sprint(plat.CalculateTotalLoad())
 }
 
 type Platform [][]rune
+
+func (p Platform) Key() string {
+	var key string
+	for _, row := range p {
+		for _, val := range row {
+			key += string(val)
+		}
+	}
+	return key
+}
 
 func (p Platform) TiltNorth() {
 	for y, row := range p {
@@ -29,11 +66,65 @@ func (p Platform) TiltNorth() {
 			if val == '.' {
 				// Search for any Os in the column (above a #)
 				// And move to this location if so.
-				for rowIdx := y; rowIdx < len(p); rowIdx++ {
+				for rowIdx := y + 1; rowIdx < len(p); rowIdx++ {
 					if p[rowIdx][x] == '#' {
 						break
 					} else if p[rowIdx][x] == 'O' {
 						p[rowIdx][x] = '.'
+						p[y][x] = 'O'
+						break
+					}
+				}
+			}
+		}
+	}
+}
+
+func (p Platform) TiltSouth() {
+	for y := len(p) - 1; y >= 0; y-- {
+		for x, val := range p[y] {
+			if val == '.' {
+				for rowIdx := y - 1; rowIdx >= 0; rowIdx-- {
+					if p[rowIdx][x] == '#' {
+						break
+					} else if p[rowIdx][x] == 'O' {
+						p[rowIdx][x] = '.'
+						p[y][x] = 'O'
+						break
+					}
+				}
+			}
+		}
+	}
+}
+
+func (p Platform) TiltEast() {
+	for x := len(p[0]) - 1; x >= 0; x-- {
+		for y := 0; y < len(p); y++ {
+			if p[y][x] == '.' {
+				for colIdx := x - 1; colIdx >= 0; colIdx-- {
+					if p[y][colIdx] == '#' {
+						break
+					} else if p[y][colIdx] == 'O' {
+						p[y][colIdx] = '.'
+						p[y][x] = 'O'
+						break
+					}
+				}
+			}
+		}
+	}
+}
+
+func (p Platform) TiltWest() {
+	for x := 0; x < len(p[0]); x++ {
+		for y := 0; y < len(p); y++ {
+			if p[y][x] == '.' {
+				for colIdx := x + 1; colIdx < len(p[0]); colIdx++ {
+					if p[y][colIdx] == '#' {
+						break
+					} else if p[y][colIdx] == 'O' {
+						p[y][colIdx] = '.'
 						p[y][x] = 'O'
 						break
 					}
--- day14/day14_test.go
+++ day14/day14_test.go
@@ -1,6 +1,7 @@
 package day14
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/mikeconroy/advent-of-code-23/utils"
@@ -15,8 +16,8 @@ func TestDay14Part1(t *testing.T) {
 
 func TestDay14Part2(t *testing.T) {
 	input := utils.ReadFileIntoSlice("input_test")
-	if part2(input) != "0" {
-		t.Fatal("Day 14 - Part 2 output should be xxx")
+	if part2(input) != "64" {
+		t.Fatal("Day 14 - Part 2 output should be 64")
 	}
 }
 
@@ -37,5 +38,109 @@ func TestCalculateTotalLoad(t *testing.T) {
 
 	if actual != 136 {
 		t.Fatal("Day 14 - Total Load is wrong. Should be 136. Got:", actual)
+	}
+}
+
+func TestTiltSouth(t *testing.T) {
+	plat := Platform{
+		[]rune("OOOO.#.O.."),
+		[]rune("OO..#....#"),
+		[]rune("OO..O##..O"),
+		[]rune("O..#.OO..."),
+		[]rune("........#."),
+		[]rune("..#....#.#"),
+		[]rune("..O..#.O.O"),
+		[]rune("..O......."),
+		[]rune("#....###.."),
+		[]rune("#....#...."),
+	}
+	plat.TiltSouth()
+
+	expected := Platform{
+		[]rune(".....#...."),
+		[]rune("....#....#"),
+		[]rune("...O.##..."),
+		[]rune("...#......"),
+		[]rune("O.O....O#O"),
+		[]rune("O.#..O.#.#"),
+		[]rune("O....#...."),
+		[]rune("OO....OO.."),
+		[]rune("#OO..###.."),
+		[]rune("#OO.O#...O"),
+	}
+
+	if !reflect.DeepEqual(plat, expected) {
+		plat.Print()
+		expected.Print()
+		t.Fatal("Day 14 - Tilt South is not correct.")
+	}
+}
+
+func TestTiltEast(t *testing.T) {
+	plat := Platform{
+		[]rune("OOOO.#.O.."),
+		[]rune("OO..#....#"),
+		[]rune("OO..O##..O"),
+		[]rune("O..#.OO..."),
+		[]rune("........#."),
+		[]rune("..#....#.#"),
+		[]rune("..O..#.O.O"),
+		[]rune("..O......."),
+		[]rune("#....###.."),
+		[]rune("#....#...."),
+	}
+	plat.TiltEast()
+
+	expected := Platform{
+		[]rune(".OOOO#...O"),
+		[]rune("..OO#....#"),
+		[]rune("..OOO##..O"),
+		[]rune("..O#....OO"),
+		[]rune("........#."),
+		[]rune("..#....#.#"),
+		[]rune("....O#..OO"),
+		[]rune(".........O"),
+		[]rune("#....###.."),
+		[]rune("#....#...."),
+	}
+
+	if !reflect.DeepEqual(plat, expected) {
+		plat.Print()
+		expected.Print()
+		t.Fatal("Day 14 - Tilt East is not correct.")
+	}
+}
+func TestTiltWest(t *testing.T) {
+	plat := Platform{
+		[]rune("OOOO.#.O.."),
+		[]rune("OO..#....#"),
+		[]rune("OO..O##..O"),
+		[]rune("O..#.OO..."),
+		[]rune("........#."),
+		[]rune("..#....#.#"),
+		[]rune("..O..#.O.O"),
+		[]rune("..O......."),
+		[]rune("#....###.."),
+		[]rune("#....#...."),
+	}
+	plat.TiltWest()
+
+	expected := Platform{
+		[]rune("OOOO.#O..."),
+		[]rune("OO..#....#"),
+		[]rune("OOO..##O.."),
+		[]rune("O..#OO...."),
+		[]rune("........#."),
+		[]rune("..#....#.#"),
+		[]rune("O....#OO.."),
+		[]rune("O........."),
+		[]rune("#....###.."),
+		[]rune("#....#...."),
+	}
+
+	if !reflect.DeepEqual(plat, expected) {
+		plat.Print()
+		expected.Print()
+		t.Fatal("Day 14 - Tilt West is not correct.")
 	}
 }

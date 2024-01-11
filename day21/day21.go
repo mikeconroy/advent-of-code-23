ff2c5920bcbd8f161cea092f9d871038e7a0a44d
Mike Conroy <mikecon@outlook.com>
2024-01-11 21:26:52 +0000

    Day 21 - Part 2

--- day21/day21.go
+++ day21/day21.go
@@ -2,13 +2,14 @@ package day21
 
 import (
 	"fmt"
+	"math"
 
 	"github.com/mikeconroy/advent-of-code-23/utils"
 )
 
 func Run() (string, string) {
 	input := utils.ReadFileIntoSlice("day21/input")
-	return part1(input, 64), part2(input)
+	return part1(input, 64, 131), part2(input, 131)
 }
 
 type Point struct {
@@ -22,18 +23,18 @@ const (
 	ROCK
 )
 
-func part1(input []string, steps int) string {
+func part1(input []string, steps int, length int) string {
 	plots, start := loadMap(input)
 
 	plotsReached := make(map[Point]bool)
 	plotsReached[start] = true
 	for step := 0; step < steps; step++ {
-		plotsReached = processStep(plotsReached, plots)
+		plotsReached = processStep(plotsReached, plots, length)
 	}
 	return fmt.Sprint(len(plotsReached))
 }
 
-func processStep(positions map[Point]bool, plots map[Point]Plot) map[Point]bool {
+func processStep(positions map[Point]bool, plots map[Point]Plot, length int) map[Point]bool {
 	newPositions := make(map[Point]bool)
 	for pos, _ := range positions {
 		neighbours := []Point{
@@ -43,7 +44,19 @@ func processStep(positions map[Point]bool, plots map[Point]Plot) map[Point]bool 
 			{x: pos.x + 1, y: pos.y},
 		}
 		for _, neighbour := range neighbours {
-			if plot, ok := plots[neighbour]; ok && plot == GARDEN && !newPositions[neighbour] {
+			neighbourOffset := Point{x: neighbour.x % length, y: neighbour.y % length}
+			if neighbourOffset.x < 0 {
+				neighbourOffset.x = length + neighbourOffset.x
+			}
+
+			if neighbourOffset.y < 0 {
+				neighbourOffset.y = length + neighbourOffset.y
+			}
+
+			// if (len(positions) == 16 || len(positions) == 22) && neighbour != neighbourOffset {
+			// 	fmt.Println("Original:", neighbour, "Translated:", neighbourOffset, "Type:", plots[neighbourOffset])
+			// }
+			if plot, ok := plots[neighbourOffset]; ok && plot == GARDEN && !newPositions[neighbour] {
 				newPositions[neighbour] = true
 			}
 		}
@@ -52,11 +65,34 @@ func processStep(positions map[Point]bool, plots map[Point]Plot) map[Point]bool 
 }
 
 // 26_501_365 steps with the map repeating in all directions infinitely.
-// We only care about the outer points - the other points just flip-flop on and off between themselves.
-// Outer points continue to expand and move outwards increasing the of plots reached.
-func part2(input []string) string {
-	// fmt.Println(part1(input, 200))
-	return fmt.Sprint(0)
+// https://www.reddit.com/r/adventofcode/comments/18orn0s
+// https://www.reddit.com/r/adventofcode/comments/18oh5f7/comment/keh27rk
+// https://www.dcode.fr/lagrange-interpolating-polynomial
+// (65, 3867) (196, 34253) (327, 94909) -> 619407349431167
+func part2(input []string, length int) string {
+	plots, start := loadMap(input)
+	plotsReached := make(map[Point]bool)
+	plotsReached[start] = true
+	var y0, y1, y2 int
+	for step := 1; step <= 327; step++ {
+		plotsReached = processStep(plotsReached, plots, length)
+		if step == 65 {
+			y0 = len(plotsReached)
+		} else if step == (131 + 65) {
+			y1 = len(plotsReached)
+		} else if step == ((2 * 131) + 65) {
+			y2 = len(plotsReached)
+		}
+	}
+
+	// y0 := 3867
+	// y1 := 34253
+	// y2 := 94909
+
+	x := (26_501_365 - 65) / 131
+
+	res := ((y0 * (int(math.Pow(float64(x), 2)) - (3 * x) + 2)) / 2) - (y1 * (int(math.Pow(float64(x), 2)) - (2 * x))) + (y2 * (int(math.Pow(float64(x), 2)) - x) / 2)
+	return fmt.Sprint(res)
 }
 
 func loadMap(in []string) (plots map[Point]Plot, start Point) {
--- day21/day21_test.go
+++ day21/day21_test.go
@@ -8,14 +8,14 @@ import (
 
 func TestDay21Part1(t *testing.T) {
 	input := utils.ReadFileIntoSlice("input_test")
-	if part1(input, 6) != "16" {
+	if part1(input, 6, 11) != "16" {
 		t.Fatal("Day 21 - Part 1 output should be 16")
 	}
 }
 
-func TestDay21Part2(t *testing.T) {
-	input := utils.ReadFileIntoSlice("input_test")
-	if part2(input) != "0" {
-		t.Fatal("Day 21 - Part 2 output should be xxx")
-	}
-}
+// func TestDay21Part2(t *testing.T) {
+// 	input := utils.ReadFileIntoSlice("input_test")
+// 	if part2(input, 11) != "0" {
+// 		t.Fatal("Day 21 - Part 2 output should be xxx")
+// 	}
+// }
